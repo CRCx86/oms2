a7df4f42d059dfa815c4b167553178c1f8112a30
CRCx86 <crc32zinov@gmail.com>
2021-10-27 14:59:12 +0300

    - оптимизация инициализации миграции

--- migrations/2021-10-25-14-10-_init_ref_tables.sql
+++ migrations/2021-10-25-14-10-_init_ref_tables.sql
@@ -1,5 +1,82 @@
 -- liquibase formatted sql
 
+-- changeset zinov:2021-10-24-14-53-_Ref_ET
+CREATE TABLE IF NOT EXISTS _Ref_ET
+(
+    id   bigserial  NOT NULL,
+    name varchar NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO _Ref_ET(name) VALUES('event_type1'), ('event_type2');
+-- rollback drop table _Ref_ET;
+
+-- changeset zinov:2021-10-25-14-08-_Ref_O
+CREATE TABLE IF NOT EXISTS _Ref_O
+(
+    id   bigserial NOT NULL,
+    name varchar   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO _Ref_O(name) VALUES('order1'), ('order2');
+-- rollback drop table _Ref_O;
+
+-- changeset zinov:2021-10-25-14-10-_Ref_D
+CREATE TABLE IF NOT EXISTS _Ref_D
+(
+    id   bigserial NOT NULL,
+    name varchar   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO _Ref_D(name) VALUES('delivery1'), ('delivery1');
+-- rollback drop table _Ref_D;
+
+-- changeset zinov:2021-10-25-14-09-_Ref_S
+CREATE TABLE IF NOT EXISTS _Ref_S
+(
+    id   bigserial NOT NULL,
+    name varchar   NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO _Ref_S(name) VALUES('shipment1'), ('shipment1');
+-- rollback drop table _Ref_S;
+
+-- changeset zinov:2021-10-25-14-06-_Ref_L
+CREATE TABLE IF NOT EXISTS _Ref_L
+(
+    id   bigserial NOT NULL,
+    name varchar   NOT NULL,
+    order_id int NOT NULL ,
+    foreign key(order_id) references _Ref_O(id) on delete cascade,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO _Ref_L(name, order_id) VALUES ('lot1', 1), ('lot2', 2);
+-- rollback drop table _Ref_L;
+
+-- changeset zinov:2021-10-25-14-07-_Ref_M
+CREATE TABLE IF NOT EXISTS _Ref_M
+(
+    id              bigserial NOT NULL,
+    name            varchar   NOT NULL,
+    type            varchar NOT NULL,
+    action          varchar NOT NULL,
+    event_trigger   int,
+    waiting_time    int,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO _Ref_M(name, type, action, event_trigger, waiting_time)
+VALUES ('node1', 'action', 'FirstInit', null, 0),
+       ('node2', 'action', 'SecondInit', null, 0),
+       ('node3', 'wait', 'Wait', null, 120),
+       ('node4', 'trigger', 'Trigger', 1, 0),
+       ('node5', 'terminate', 'Terminate', null, 0);
+-- rollback drop table _Ref_M;
+
 -- changeset zinov:2021-10-25-14-11-1
 CREATE TABLE _Ref_E
 (
